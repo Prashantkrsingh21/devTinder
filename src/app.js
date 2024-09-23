4922e602242b9b8eaff0e7e430173ba54dbdfc5f
Prashantkrsingh21 <prashantkrsingh.1998@gmail.com>
2024-09-23 18:00:53 +0530

    Add delete, update, get, post api

--- src/app.js
+++ src/app.js
@@ -18,6 +18,70 @@ app.post("/signup", async (req, res) => {
     }
 })
 
+app.get("/users", async (req, res) => {
+    // const email  = req.body.email;
+    try {
+        const users = await User.find({})
+        if (users.length === 0) {
+            res.status(404).send("No users are there currently in the system");
+        }
+        res.send(users)
+    } catch (error) {
+        res.status(500).send("Something went wrong");
+    }
+})
+
+app.get("/userbyemail", async (req, res) => {
+    const email = req.body.email;
+    try {
+        const users = await User.findOne({ email })
+        if (!users) {
+            res.status(404).send("No users are there currently in the system");
+        }
+        else {
+            res.send(users)
+        }
+    } catch (error) {
+        res.status(500).send("Something went wrong");
+    }
+})
+
+app.delete("/user", async (req, res) => {
+    const userId = req.body.userId;
+    try {
+        const deletedUser = await User.findByIdAndDelete(userId)
+        if (!deletedUser) {
+            res.send("User is not there in the database")
+        }
+        else {
+            res.send("User deleted successfully")
+        }
+    } catch (error) {
+
+    }
+})
+
+
+app.patch("/user", async (req, res) => {
+    const userId = req.body.userId;
+    const email = req.body.email;
+    const data = req.body;
+
+    try {
+        // const updatedUser = await User.findByIdAndUpdate(userId, data);
+        const updatedUser = await User.findOneAndUpdate({ email }, data);
+        if (!updatedUser) {
+            res.send("User is not there in the database")
+        }
+        else {
+            res.send("User updated successfully")
+        }
+
+    } catch (error) {
+        res.status(500).send("Something went wrong");
+    }
+})
+
 connectDB().then(() => {
     console.log("Connection established to database");
     app.listen(7777, () => {
